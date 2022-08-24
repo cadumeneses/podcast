070c62350867518126db6caaa2aa0c18ebd581c9
Carlos Eduardo <carloseduardoms646@gmail.com>
2022-08-24 10:10:57 -0300

    app podcast

---
+++ lib/app/util/custom_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const purple700 = Color(0xFF292537);
+  static const pink700 = Color(0xFFe4105d);
+  static const blue750 = Color(0xFF249fef);
+  static const purple750 = Color(0xFF5f53d5);
+  static const gray700 = Color(0xFF1e192c);
+  static const gray200 = Color(0xFFa0a4ae);
+  static const blue200 = Color(0xFFeceff7);
+}
